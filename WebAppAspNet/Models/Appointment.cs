97c678b131b82856a2ca6b543d8c903d64d9447a
Kolia <33322021+KoliaHorash@users.noreply.github.com>
2018-06-19 15:29:03 +0300

    Add files via upload
    Date displayed incorrectly

--- WebAppAspNet/Models/Appointment.cs
+++ WebAppAspNet/Models/Appointment.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,13 +12,15 @@ namespace WebAppAspNet.Models
         private DateTime dateEndOfActuality;
 
         public int ID { get; set; }
-        public DateTime DateCteate {
-            get {return dateCreate.Date; }
-            set {dateCreate = value.Date; }
+        public DateTime DateCteate
+        {
+            get {return Convert.ToDateTime(dateCreate.ToString("d.M.yyy", CultureInfo.InvariantCulture)); }
+            set {dateCreate = Convert.ToDateTime(value.ToString("d.M.yyy", CultureInfo.InvariantCulture)); }
         }
-        public DateTime DateEndOfActuality {
-            get {return dateEndOfActuality.Date; }
-            set {dateEndOfActuality = value.Date; }
+        public DateTime DateEndOfActuality
+        {
+            get {return Convert.ToDateTime(dateEndOfActuality.ToString("d.M.yyy", CultureInfo.InvariantCulture)); }
+            set { dateEndOfActuality = Convert.ToDateTime(value.ToString("d.M.yyy", CultureInfo.InvariantCulture)); }
         }
         public int CategoryId { get; set; }
         public string Description { get; set; }
